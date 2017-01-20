db3071a7c2501149d0d867a9ca6853c6407ff53d
Ever Garcia <uriel621@live.com>
2017-01-19 16:55:02 -0800

    message list query with mongo

--- back_end/server.js
+++ back_end/server.js
@@ -1,9 +1,12 @@
 var express = require("express")
 var app = express();
 var bodyParser = require("body-parser");
-var mongo = require("mongodb").MongoClient;
+var mongoose = require("mongoose")
 
 var database;
+var Message = mongoose.model("message", {
+    msg: String
+})
 
 app.use(bodyParser.json());
 
@@ -14,17 +17,27 @@ app.use(function(req, res, next){
 
 })
 
+app.post("/api/message", GetMessage)
+
 app.post("/api/message", function(req, res){
     console.log(req.body);
-    database.collection("messages").insertOne(req.body)
+    var message = new Message(req.body) //req contains the message
+    message.save()
     res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err, db){
+function GetMessage (req, res){
+    Message.find({}).exec(function(err, result){
+        // console.log(result)
+        res.send(result)
+    })
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err, db){
     if(!err){
         console.log("We are connected to Mongo")
         // db.collection("messages").insertOne({"msg": "test"})
-        database = db;
+        GetMessage()
     }
 })
 

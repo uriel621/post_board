faaf094e8d2bdf01f74d8122875498bb15916d82
Ever Garcia <uriel621@live.com>
2017-01-19 17:32:26 -0800

    display the messages to the DOM

--- front_end/src/app/main/main.controller.js
+++ front_end/src/app/main/main.controller.js
@@ -1,10 +1,21 @@
 export class MainController {
   constructor ($http) {
     'ngInject';
-
+    
     this.$http = $http
+    this.getMessages()
 
   }
+  getMessages(){
+      console.log("post")
+      //viewmodel
+      var vm = this
+      this.$http.get("http://localhost:5000/api/message").then(function(result){
+        console.log(result)
+        vm.messages = result.data;
+      })
+    }
+  
   postMessage(){
     console.log("post")
     this.$http.post("http://localhost:5000/api/message", {msg: this.message})
